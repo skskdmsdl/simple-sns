f4094ac1257528a67127ba972cf766193006c293
skskdmsdl <skskdmsdl@gmail.com>
2022-08-10 17:46:26 +0900

    feat : add RuntimeException code

--- src/main/java/com/study/sns/exception/ErrorCode.java
+++ src/main/java/com/study/sns/exception/ErrorCode.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     ;
 
     private HttpStatus status;
