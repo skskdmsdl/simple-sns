cdac91c3a5bf6c29f89028a4e460c8aa51628d2f
skskdmsdl <skskdmsdl@gmail.com>
2022-08-11 13:33:09 +0900

    feat : add post code and test post code

---
+++ src/main/java/com/study/sns/model/entity/PostEntity.java
@@ -0,0 +1,54 @@
+package com.study.sns.model.entity;
+
+import com.study.sns.model.UserRole;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Table(name = "post")
+@Setter
+@Getter
+@SQLDelete(sql = "UPDATE post SET deleted_at = NOW() where id=?")
+@Where(clause = "deleted_at is NULL")
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "body", columnDefinition = "TEXT") // columnDefinition = 컬럼 타입 변경
+    private String body;
+
+    // UserEntity에서 post를 가져올 때 user_id로 user 테이블과 join 해서 가져옴
+    @ManyToOne // 하나의 유저가 여러 post 쓸 수 있음
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+}
