319a017911dd7bc62993c57af5e8934f2abad40a
skskdmsdl <skskdmsdl@gmail.com>
2022-08-14 16:16:18 +0900

    feat : add post modify api

--- src/main/java/com/study/sns/controller/PostController.java
+++ src/main/java/com/study/sns/controller/PostController.java
@@ -1,14 +1,14 @@
 package com.study.sns.controller;
 
 import com.study.sns.controller.request.PostCreateRequest;
+import com.study.sns.controller.request.PostModifyRequest;
+import com.study.sns.controller.response.PostResponse;
 import com.study.sns.controller.response.Response;
+import com.study.sns.model.Post;
 import com.study.sns.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -21,5 +21,11 @@ public class PostController {
     public Response<Void> create(@RequestBody PostCreateRequest request, Authentication authentication) {
         postService.create(request.getTitle(), request.getBody(), authentication.getName());
         return Response.success();
+    }
+
+    @PutMapping("/{postId}")
+    public Response<PostResponse> modify(@PathVariable Integer postId, @RequestBody PostModifyRequest request, Authentication authentication) {
+        Post post = postService.modify(request.getTitle(), request.getBody(), authentication.getName(), postId);
+        return Response.success(PostResponse.fromPost(post));
     }
 }
--- src/main/java/com/study/sns/service/PostService.java
+++ src/main/java/com/study/sns/service/PostService.java
@@ -2,6 +2,7 @@ package com.study.sns.service;
 
 import com.study.sns.exception.ErrorCode;
 import com.study.sns.exception.SnsApplicationException;
+import com.study.sns.model.Post;
 import com.study.sns.model.entity.PostEntity;
 import com.study.sns.model.entity.UserEntity;
 import com.study.sns.repository.PostEntityRepository;
@@ -28,11 +29,21 @@ public class PostService {
         PostEntity saved = postEntityRepository.save(PostEntity.of(title, body, userEntity));
     }
 
-    public void modify(String title, String body, String userName, Integer postId) {
+    public Post modify(String title, String body, String userName, Integer postId) {
         UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() ->
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
         // post exist
+        PostEntity postEntity = postEntityRepository.findById(postId).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
 
         // post permission
+        if(postEntity.getUser() != userEntity) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission with %s", userName, postId));
+        }
+
+        postEntity.setTitle(title);
+        postEntity.setBody(body);
+
+        return  Post.fromEntity(postEntityRepository.saveAndFlush(postEntity));
     }
 }
