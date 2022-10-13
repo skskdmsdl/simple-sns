6a8946df1ff097df092ac5ffa0b05272b78e3eb3
skskdmsdl <skskdmsdl@gmail.com>
2022-10-14 00:09:40 +0900

    error code 세팅

--- src/main/java/com/study/sns/exception/ErrorCode.java
+++ src/main/java/com/study/sns/exception/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+    ALREADY_LIKED_POST(HttpStatus.CONFLICT, "user already like the post"),
     ;
 
     private HttpStatus status;
