43d9dc13fca03650baaa3bbd7fe349f5400691f2
skskdmsdl <skskdmsdl@gmail.com>
2022-11-15 22:36:27 +0900

    redis dependency 추가 및 configuration 파일 작성

---
+++ src/main/java/com/study/sns/configuration/RedisConfiguration.java
@@ -0,0 +1,9 @@
+package com.study.sns.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfiguration {
+}
