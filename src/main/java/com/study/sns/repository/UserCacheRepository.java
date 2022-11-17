e54a30f77a053f2dd9d2077c8eae6b0a716fb237
skskdmsdl <skskdmsdl@gmail.com>
2022-11-17 21:16:49 +0900

    redis cache repository 클래스 작성

---
+++ src/main/java/com/study/sns/repository/UserCacheRepository.java
@@ -0,0 +1,40 @@
+package com.study.sns.repository;
+
+import com.study.sns.model.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+// redis의 유저를 캐싱하고 그 캐시에서 유저를 가져오는 클래스
+// redis 같은 경우 일정 시간이 지나면 expired 되도록 하는 것이 좋음(TTL)
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class UserCacheRepository {
+
+    private final RedisTemplate<String, User> userRedisTemplate;
+
+    public void setUser(User user) {
+        String key = getKey(user.getUsername());
+        log.info("Set User to Redis {}:{}", key, user);
+        userRedisTemplate.opsForValue().set(key, user);
+    }
+
+    public User getUser(String userName) {
+        String key = getKey(userName);
+        User user = userRedisTemplate.opsForValue().get(key);
+        log.info("Get data from Redis {}:{}", key, user);
+        return user;
+    }
+
+    // 가장 많이 사용하는 필터부분(유저 확인을 위해)을 캐시로 대체하려함
+    // JwtTokenFilter에 유저 가져오는 부분 userName으로 가져오기에 여기서도 userName으로 설정
+    private String getKey(String userName) {
+
+        // redis에 key값을 구성할때는 항상 prefix를 붙여서 사용
+        return "USER:" + userName;  // USER:admin admin이라는 userName에 USER정보를 저장한다는걸 표현하기 위함
+    }
+
+
+}
