c378b149312876905b456ddf45c445a9253689cd
skskdmsdl <skskdmsdl@gmail.com>
2022-11-27 20:24:50 +0900

    send 메서드 추가

--- src/main/java/com/study/sns/service/AlarmService.java
+++ src/main/java/com/study/sns/service/AlarmService.java
@@ -2,16 +2,32 @@ package com.study.sns.service;
 
 import com.study.sns.exception.ErrorCode;
 import com.study.sns.exception.SnsApplicationException;
+import com.study.sns.repository.EmitterRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class AlarmService {
 
     private final static Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
     private final static String ALARM_NAME = "alarm";
+    private final EmitterRepository emitterRepository;
+
+    public void send(Integer alarmId, Integer userId) {
+        emitterRepository.get(userId).ifPresentOrElse(sseEmitter -> {
+            try {
+                sseEmitter.send(SseEmitter.event().id(alarmId.toString()).name(ALARM_NAME).data("new alarm"));
+            } catch (IOException e) {
+                throw new SnsApplicationException(ErrorCode.ALARM_CONNECT_ERROR);
+            }
+        }, () -> log.info("No emitter founded"));
+    }
 
     public SseEmitter connectAlarm(Integer userId) {
         SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
